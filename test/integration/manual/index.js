c347441375e6f7f9525bf1238b74a55ec3650bfa
Laszlo Simon <simon@vizzuhq.com>
2021-07-22 11:44:11 +0200

    Integration test manual checking page.

---
+++ test/integration/manual/index.js
@@ -0,0 +1,55 @@
+let vizzuUrl = document.getElementById('vizzuUrl');
+let testCase = document.getElementById('testCase');
+let frame = document.getElementById('frame');
+
+function update() {
+	localStorage.setItem('vizzuUrl', vizzuUrl.value);
+	localStorage.setItem('testCase', testCase.value);
+	frame.src = `../modules/client/index.html?testCase=${testCase.value}&vizzuUrl=${vizzuUrl.value}`;
+}
+
+function setupSelects()
+{
+	vizzuUrl.addEventListener("change", update);
+	testCase.addEventListener("change", update);
+}
+
+function populateLibs()
+{
+	fetch('liblist.json')
+	.then(response => response.json())
+	.then(data => {
+		for (let name in data)
+		{
+			let url = data[name];
+			vizzuUrl.innerHTML += `<option value='${url}'>${name}</option>`;
+		}
+		let lastSelected = localStorage.getItem('vizzuUrl');
+		if (lastSelected === '') lastSelected = data['localhost'];
+		vizzuUrl.value = lastSelected;
+		populateCases();
+	});
+
+}
+
+function populateCases()
+{
+	fetch('testlist.json')
+	.then(response => response.json())
+	.then(data => {
+		for (let i = 0; i < data.length; i++)
+		{
+			let actcase = data[i];
+			let selected = i == 0 ? 'selected="selected"' : '';
+			testCase.innerHTML += `<option ${selected} value='${actcase}'>${actcase}</option>`;
+		}
+		let lastSelected = localStorage.getItem('testCase');
+		if (lastSelected === '') lastSelected = data[0];
+		testCase.value = lastSelected;
+
+		setupSelects();
+		update();
+	});
+}
+
+populateLibs();
