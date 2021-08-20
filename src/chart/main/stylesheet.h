2ea8b01477f89d94c732640c7f4570a6fb860ec4
Laszlo Simon <simon@vizzuhq.com>
2021-08-20 10:51:22 +0200

    Styles::Sheet class for option dependent style defaults added.

--- src/chart/main/stylesheet.h
+++ src/chart/main/stylesheet.h
@@ -2,13 +2,32 @@
 #define STYLESHEET_H
 
 #include "base/style/sheet.h"
+#include "chart/options/options.h"
 #include "style.h"
 
 namespace Vizzu
 {
+namespace Styles
+{
 
-typedef Style::Sheet<Styles::Chart> Stylesheet;
+class Sheet : public Style::Sheet<Chart>
+{
+public:
+	typedef Style::Sheet<Chart> Base;
+	using Base::Sheet;
 
+	Chart getFullParams(const Diag::DiagramOptionsPtr &options);
+
+private:
+	using Base::getFullParams;
+
+	const Diag::Options *options;
+
+	void calcDefaults();
+	void calcFinals(Chart &params);
+};
+
+}
 }
 
 #endif
