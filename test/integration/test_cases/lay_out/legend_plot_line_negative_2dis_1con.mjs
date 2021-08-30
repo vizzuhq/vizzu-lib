5a8f96923d8092ebbcb49365515a72e9160cd1c8
andraskangyal <akangyal@gmail.com>
2021-08-30 16:03:51 +0200

    Test case lay out testing

---
+++ test/integration/test_cases/lay_out/legend_plot_line_negative_2dis_1con.mjs
@@ -0,0 +1,22 @@
+import { data } from '/test/integration/test_data/chart_types_eu.js';
+
+const testSteps = [
+    chart => chart.animate(
+        {
+            data: data,
+            descriptor:
+            {
+                channels:
+                {
+                    y: { attach: ['Negative nums'], range: '0,1.1,%'},
+                    x: { attach: ['Year']},
+                    color: { attach: ['Country']}
+                },
+                title: null,
+                geometry: 'line'
+            }
+        }
+    )
+];
+
+export default testSteps;
