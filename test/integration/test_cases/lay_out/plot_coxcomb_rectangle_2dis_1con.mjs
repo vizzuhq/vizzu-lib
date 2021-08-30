56adc8f16dc4ba5c0f55e607b4c19dceb42616ee
andraskangyal <akangyal@gmail.com>
2021-08-30 16:03:51 +0200

    Test case lay out testing

---
+++ test/integration/test_cases/lay_out/plot_coxcomb_rectangle_2dis_1con.mjs
@@ -0,0 +1,37 @@
+import { data } from '/test/integration/test_data/chart_types_eu.js';
+
+const testSteps = [
+    chart => chart.animate(
+        {
+            data: data,
+            descriptor:
+            {
+                channels:
+                {
+                    y: { attach: ['Threat', 'Positive nums'], range: '0,1.075,%' },
+                    x: { attach: ['Year'] },
+                    color: { attach: ['Threat'] }
+                },
+                title: null,
+                coordSystem: 'polar',
+                legend: null
+            },
+            style: 
+            {
+                data: 
+                { 
+                    columnMaxPadding: 0.21 
+                },
+                plot: 
+                { 
+                    marker: 
+                    {
+                        borderOpacity:1
+                    } 
+                }
+            }
+        }
+    )
+];
+
+export default testSteps;
