a6a85f8bf26aeb7cdf2690342fd71ff972e3f15a
andraskangyal <akangyal@gmail.com>
2021-09-04 17:36:48 +0200

    Content. New content anim.

---
+++ test/integration/test_cases/web_content/templates/orientation_marimekko_rectangle_2dis_2con.mjs
@@ -0,0 +1,60 @@
+import { data } from '/test/integration/test_data/chart_types_eu.js';
+
+data.filter = record => 
+record.Country == 'Bulgaria' || 
+record.Country == 'Germany' || 
+record.Country == 'Malta' || 
+record.Country == 'Lithuania'
+
+const testSteps = [
+    chart => chart.animate(
+        {
+            data: data,
+            config:
+            {
+                channels:
+                {
+                    y: { attach: ['Joy factors', 'Value 2 (+)'] },
+                    x: { attach: ['Country', 'Value 2 (+)'] },
+                    color: { attach: ['Joy factors'] },
+                    label: { attach: ['Country', 'Value 2 (+)'] },
+                    lightness: { attach: ['Country'] }
+                },
+                title: 'Marimekko Chart',
+                align: 'stretch'
+            },
+            style:
+            {
+                plot:
+                {
+                    marker:
+                    {
+                        label:
+                        {
+                            format: 'categoriesFirst'
+                        }
+                    }
+                }
+            }
+        }
+    ),
+    chart => chart.animate(
+        {
+            config:
+            {
+                channels:
+                {
+                    y: { attach: ['Joy factors', 'Value 2 (+)'] },
+                    x: { attach: ['Country', 'Value 2 (+)'] },
+                    color: { attach: ['Joy factors'] },
+                    label: { attach: ['Country', 'Value 2 (+)'] }
+                },
+                title: 'Marimekko Chart',
+                align: 'stretch',
+                orientation: 'vertical'
+            }
+        }
+    )
+];
+
+export default testSteps;
