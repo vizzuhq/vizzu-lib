a2e267b9538fe9d910100aab5669f3804ec0e69c
Laszlo Simon <simon@vizzuhq.com>
2021-09-07 18:59:38 +0200

    neo-theme added to typedoc reference generator.

--- tools/refgen/gen.sh
+++ tools/refgen/gen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-typedoc --out ../../docs/reference ../../src/apps/weblib/js-api/vizzu.d.ts
+typedoc --theme ./node_modules/typedoc-neo-theme/bin/default --out ../../docs/reference ../../src/apps/weblib/js-api/vizzu.d.ts
