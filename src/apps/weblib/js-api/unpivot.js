b411c68c6c8e83f52323053a26101e8d760294c4
David Vegh <david.andras.vegh+github@gmail.com>
2021-09-08 17:25:56 +0200

    unpivot: - validdate inner values size - validate object, list types - rename values to measures - measures could be a simple object - use measure type

--- src/apps/weblib/js-api/unpivot.js
+++ src/apps/weblib/js-api/unpivot.js
@@ -7,64 +7,96 @@ const assert = (condition, message) => {
     }
 };
 
+
+const assertArray = (data, array, index) => {
+    assert(Array.isArray(array), 'array is not a list');
+    try {
+        if (array.length !== data.dimensions[index].values.length) {
+            throw new Error('Assert failed');
+        }
+    } catch (err) {
+        assert(false, 'dimensions are not the same');
+    }
+    array.forEach(item => {
+        if (Array.isArray(item)) {
+            assertArray(data, item, index - 1);
+        }
+    });
+};
+
+
 export default class UnPivot {
 
     static isPivot(data) {
         return data.dimensions !== undefined
-            || data.values !== undefined;
+            || data.measures !== undefined;
     }
 
     static convert(data) {
+        assert(typeof data === 'object' && data !== null && !Array.isArray(data), 'data is not an object');
         assert('dimensions' in data, 'dimensions field is requreid');
-        assert('values' in data, 'values field is requreid');
-        assert(!('series' in data), 'both series and dimensions/values cannot be set');
-        assert(!('records' in data), 'both series and dimensions/values cannot be set');
+        assert('measures' in data, 'measures field is requreid');
+        assert(!('series' in data), 'both series and dimensions/measures cannot be set');
 
         data.series = [];
 
         let dimensionsProduct = 1;
+        assert(Array.isArray(data.dimensions), 'data.dimensions is not a list');
         for (let i = 0; i < data.dimensions.length; i++) {
-            let element = data.dimensions[i];
-            assert('name' in element, 'name field is requreid');
-            assert('values' in element, 'values field is requreid');
-            assert(element.length != 0, 'length is zero');
-            dimensionsProduct *= element.values.length;
+            let item = data.dimensions[i];
+            assert(typeof item === 'object' && item !== null && !Array.isArray(item), 'item is not an object');
+            assert('name' in item, 'name field is requreid');
+            assert(item.type === undefined || item.type === 'categories', 'item.type is invalid');
+            assert('values' in item, 'values field is requreid');
+            assert(Array.isArray(item.values), 'item.values is not a list');
+            assert(item.values.length !== 0, 'length is zero');
+            dimensionsProduct *= item.values.length;
         }
 
         let dimensionsBefore = 1;
         for (let i = 0; i < data.dimensions.length; i++) {
-            let element = data.dimensions[i];
+            let item = data.dimensions[i];
             let values = [];
             let valuesItem = [];
-            element.values.forEach(value => {
+            item.values.forEach(value => {
                 for (let b = 1; b <= dimensionsBefore; b++) {
                     valuesItem.push(value);
                 }
             });
-            dimensionsBefore *= element.values.length;
+            dimensionsBefore *= item.values.length;
             for (let a = 1; a <= dimensionsProduct / dimensionsBefore; a++) {
                 values = values.concat(valuesItem);
             }
 
             let seriesItem = {
-                name: element.name,
-                type: 'categories',
+                name: item.name,
+                type: item.type || 'categories',
                 values: values
             };
             data.series.push(seriesItem);
         }
 
-        for (let i = 0; i < data.values.length; i++) {
-            let element = data.values[i];
-            assert('name' in element, 'name field is requreid');
-            assert('values' in element, 'values field is requreid');
-
+        assert(typeof data.measures === 'object' && data.measures !== null, 'data.measures is not a list or an object');
+        if (!Array.isArray(data.measures)) {
+            data.measures = [ data.measures ];
+        }
+        for (let i = 0; i < data.measures.length; i++) {
+            let item = data.measures[i];
+            assert(typeof item === 'object' && item !== null && !Array.isArray(item), 'item is not an object');
+            assert('name' in item, 'name field is requreid');
+            assert(item.type !== 'categories', 'item.type is invalid');
+            assert('values' in item, 'values field is requreid');
+            assert(Array.isArray(item.values), 'item.values is not a list');
+            assertArray(data, item.values, data.dimensions.length - 1);
             let seriesItem = {
-                name: element.name,
-                type: 'values',
-                values: data.values[i].values.flat(Infinity)
+                name: item.name,
+                type: item.type,
+                values: item.values.flat(Infinity)
             };
-            assert(seriesItem.values.length == dimensionsProduct, 'dimensions are not the same');
+            if (seriesItem.type === undefined) {
+                delete seriesItem.type;
+            }
+            assert(seriesItem.values.length === dimensionsProduct, 'dimensions are not the same');
             data.series.push(seriesItem);
         }
     }
