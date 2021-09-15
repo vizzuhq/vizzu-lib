f069d56d3fc434301e1acbeb360377bf901c65ce
Laszlo Simon <simon@vizzuhq.com>
2021-09-15 11:24:42 +0200

    VideoRecorder code added to manual tester for later use.

---
+++ test/integration/manual/videorecord.js
@@ -0,0 +1,51 @@
+"use strict";
+
+export class VideoRecord
+{
+	constructor(canvasName)
+	{
+		let recordedChunks = [];
+		let canvas = document.getElementById(canvasName);
+		let stream = canvas.captureStream(25 /*fps*/);
+		this.mediaRecorder = new MediaRecorder(stream);
+
+		this.mediaRecorder.ondataavailable = function (e) {
+			recordedChunks.push(e.data);
+		};
+
+		this.mediaRecorder.onstop = function (event) {
+			let blob = new Blob(recordedChunks, {
+				type: "video/webm"
+			});
+			let data = URL.createObjectURL(blob);
+
+			const link = document.createElement('a');
+			link.href = data;
+
+			// this is necessary as link.click() does not work on the latest firefox
+			link.dispatchEvent(
+				new MouseEvent('click', {
+					bubbles: true,
+					cancelable: true,
+					view: window
+				})
+			);
+
+			setTimeout(() => {
+				// For Firefox it is necessary to delay revoking the ObjectURL
+				window.URL.revokeObjectURL(data);
+				link.remove();
+			}, 100);
+		}
+	}
+
+	start()
+	{
+		this.mediaRecorder.start();
+	}
+
+	stop()
+	{
+		this.mediaRecorder.stop();
+	}
+}
