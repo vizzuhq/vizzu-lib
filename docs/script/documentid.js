a1935730df77b3a760915275b8e3bcd907096561
Laszlo Simon <simon@vizzuhq.com>
2021-09-16 10:26:43 +0200

    Docs: tutorial navigation fixed.

--- docs/script/documentid.js
+++ docs/script/documentid.js
@@ -1,9 +1,23 @@
 export default class DocId {
 	constructor(id) {
 		let ids = id.split('.');
-		this.document = ids[0];
-		this.section = ids[1];
-		this.subsection = ids[2];
+		this.document = Number(ids[0]);
+		this.section = Number(ids[1]);
+		this.subsection = Number(ids[2]);
+	}
+
+	next()
+	{
+		let res = new DocId(this.getSubSectionId());
+		res.subsection++;
+		return res;
+	}
+
+	prev()
+	{
+		let res = new DocId(this.getSubSectionId());
+		if (res.subsection > 0) res.subsection--;
+		return res;
 	}
 
 	getSectionId() 
@@ -23,7 +37,7 @@ export default class DocId {
 
 	sameSection(docId)
 	{
-		return this.getSectionId == docId.getSectionId();
+		return this.getSectionId() === docId.getSectionId();
 	}
 
 }
--- docs/script/vizzu-view.js
+++ docs/script/vizzu-view.js
@@ -1,4 +1,4 @@
-import Vizzu from '../../example/lib/vizzu.js';
+import Vizzu from 'https://vizzu-lib-main.storage.googleapis.com/lib/vizzu.js';
 import getBase from '../content/tutorial/base.js';
 import data from '../content/tutorial/data.js'
 
@@ -26,7 +26,11 @@ export default class VizzuView
 		}).then(chart => chart.animate({
 			data: this.data
 		})).then(chart => {
-			this.stack.push({ id: '0.0.0', snapshot: chart.store() });
+			this.stack.push({ 
+				id: '0.0.0', 
+				snapshot: chart.store(),
+				title: ''
+			});
 			return chart;
 		});
 	}
@@ -35,60 +39,132 @@ export default class VizzuView
 	{
 		this.steps.push(snippet);
 		let index = this.steps.length - 1;
-		this.stepMap.set(id, index)
+		this.stepMap.set(id, index);
+
+		let baseId = (new DocId(id)).firstInSection();
+		if (!this.stepMap.has(baseId))
+			this.register(baseId, {
+				id: baseId,
+				title: '', 
+				fn: getBase((new DocId(baseId).section))
+			});
 	}
 
-	step(id, titleSpeed = '300ms')
+	step(id)
 	{
+		console.log('step '+id);
+		let prevOfId = (new DocId(id)).prev().getSubSectionId();
+		return this.goto(prevOfId).then(() => this.stepNext(id));
+	}
+
+	stepNext(id)
+	{
+		console.log('step next'+id);
+		return this.stepTo(id, "300ms", undefined);
+	}
+
+	goto(id)
+	{
+		let last = this.stack.at(-1).id;
+		console.log('goto '+id+' from '+last);
+
+		if (id === last) return this.anim;
+
 		let index = this.stepMap.get(id);
-		if (index === undefined) return;
-/*
+		if (index === undefined) return this.anim;
+
 		let nextId = new DocId(id);
-		let lastId = new DocId(this.stack.at(-1).id);
+		let lastId = new DocId(last);
 
 		if (!nextId.sameSection(lastId))
 		{
-			if(nextId.subsection > 0)
+			if(lastId.subsection > 0)
 			{
-				let lastState = this.stack.pop();
-				
-				this.anim = this.anim
-					.then(chart => chart.animate(lastState, '250ms'))
-					.then(chart => {
-						this.step(id, titleSpeed);
-						return chart;
-					});
+				return this.stepBack().then(chart => {
+					return this.goto(id); 
+				});
 			}
 			else
 			{
-				this.anim = this.anim
-					.then(chart => {
-						let baseId = nextId.firstInSection();
-						this.step(baseId, 0).then(chart => {
-							this.step(id, titleSpeed);
-							return chart;
-						});
-
-						return chart;
-					})
+				let baseId = nextId.firstInSection();
+				return this.stepTo(baseId, null, '250ms').then(chart=> {
+					return this.goto(id); 
+				});
 			}
 		}
 		else
 		{
-*/			let code = this.steps[index];
-
-			this.anim = this.anim.then(
-				chart => chart.animate({ config: { title: code.title }}, titleSpeed));
-			
-			this.anim = this.anim.then(chart => { 
-				code.fn(chart); 
-				return chart.anim;
-			}).then(chart => {
-				this.stack.push({ id: id, snapshot: chart.store() });
-				return chart;
-			});
-/*		}
-*/
+			if (nextId.subsection === lastId.subsection)
+			{
+				return this.anim
+			}
+			if (nextId.subsection > lastId.subsection)
+			{
+				let intermediateId = lastId.next().getSubSectionId();
+				console.log(lastId+' -> '+intermediateId);
+				return this.stepTo(intermediateId, null, '250ms')
+				.then(chart => { 
+					return this.goto(id)
+				});
+			}
+			else
+			{
+				return this.stepBack().then(chart => { return this.goto(id); });
+			}
+		}
 		return this.anim;
+	}
+
+	stepTo(id, titleSpeed, animSpeed) 
+	{
+		let index = this.stepMap.get(id);
+		if (index === undefined) return this.anim;
+
+		let code = this.steps[index];
+
+		return this.stepToCode(id, code, titleSpeed, animSpeed);
+	}
+
+	stepBack(titleSpeed = null, animSpeed = '250ms')
+	{
+		return this.anim.then(chart => 
+		{
+			this.stack.pop();
+			let lastState = this.stack.at(-1);
+			return this.animTitle(lastState.title, titleSpeed)
+				.then(chart => {
+					console.log('step back' + lastState.id);
+					return chart.animate(lastState.snapshot, animSpeed)
+				});
+		});
+	}
+
+	stepToCode(id, code, titleSpeed, animSpeed)
+	{
+		return this.animTitle(code.title, titleSpeed).then(chart => 
+		{
+			if (animSpeed) chart.setAnimation(animSpeed);
+			console.log('step to code'+id);
+			code.fn(chart); 
+			return this.chart.anim;
+		})
+		.then(chart => {
+			this.stack.push({ 
+				id: id, 
+				snapshot: chart.store(), 
+				title: code.title
+			});
+			return chart;
+		});
+	}
+
+	animTitle(title, titleSpeed)
+	{
+		return this.anim.then(chart =>
+		{
+			if (titleSpeed !== null)
+				return chart.animate({ config: { title }}, titleSpeed);
+			else return chart;
+		});
 	}
 }
