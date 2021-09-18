64ef0896d5a6d8d580330510769598aa0a6eccb0
Laszlo Simon <simon@vizzuhq.com>
2021-09-18 09:34:53 +0200

    Docs: logo added back to tutorial example.

--- docs/script/vizzu-view.js
+++ docs/script/vizzu-view.js
@@ -19,11 +19,7 @@ export default class VizzuView
 
 	init()
 	{
-		this.anim = this.chart.initializing.then(chart => {
-//			chart.module._vizzu_setLogging(true);
-			chart.on('logo-draw', event => { event.preventDefault() });
-			return chart;
-		}).then(chart => chart.animate({
+		this.anim = this.chart.initializing.then(chart => chart.animate({
 			data: this.data
 		})).then(chart => {
 			this.stack.push({ 
