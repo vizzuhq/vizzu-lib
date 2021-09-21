b816d306f6cbb4a7a72143892e94eeb7e42c98f6
David Vegh <david.andras.vegh+github@gmail.com>
2021-09-21 16:48:14 +0200

    videorecorder: add resize.js

---
+++ test/integration/modules/videorecorder/resize.js
@@ -0,0 +1,40 @@
+const yargs = require('yargs');
+const path = require('path');
+const child_process = require('child_process');
+const fs = require('fs');
+
+const padLength = 7;
+
+
+try {
+
+    var argv = yargs
+        .usage('Usage: $0 [options]')
+        
+        .help('h')
+        .alias('h', 'help')
+        .version('0.0.1')
+        .alias('v', 'version')
+        .alias('s', 'size')
+        .describe('s', 'heigth')
+        .nargs('s', 1)
+        .default('s', 320)
+        .argv;
+
+        fs.mkdirSync(__dirname + '/resized', { recursive: true });
+        let files = fs.readdirSync(__dirname);
+        files.forEach(file => {
+            if (!fs.lstatSync(__dirname + '/' + file).isDirectory()) {
+                if (path.extname(file) == '.webm') {
+                    child_process.execSync('ffmpeg -i ' + file + ' -vf scale=' + argv.size.toString() + ':-1 ' + __dirname + '/resized/' + file)
+                }
+            }
+        });
+} catch (err) {
+    process.exitCode = 1;
+    let errMsg = err.toString();
+    if (err.stack !== undefined) {
+        errMsg = err.stack;
+    }
+    console.error('[ ' + 'ERROR'.padEnd(padLength, ' ') + ' ] ' + errMsg);
+}
