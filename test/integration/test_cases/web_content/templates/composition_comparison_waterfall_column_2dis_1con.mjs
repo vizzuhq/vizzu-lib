4a2bd9405d1ed5ac3b92a830884f2280fd92a808
andraskangyal <akangyal@gmail.com>
2021-09-23 15:31:03 +0200

    Web content. Minor changes in the code layout.

--- test/integration/test_cases/web_content/templates/composition_comparison_waterfall_column_2dis_1con.mjs
+++ test/integration/test_cases/web_content/templates/composition_comparison_waterfall_column_2dis_1con.mjs
@@ -12,7 +12,8 @@ const testSteps = [
                 y: { set: ['Year', 'Value 5 (+/-)'] },
                 color: {
                     set: ['Value 5 (+/-)'],
-                    range: { // Set range of colorGradient.
+                    range: { 
+                        // Set range of colorGradient.
                         min: '-45',
                         max: '45'
                     }
@@ -26,7 +27,17 @@ const testSteps = [
         style: {
             plot: {
                 marker: {
-                    colorGradient: '#3d51b8 0, #6389ec 0.15, #9fbffa 0.35, #d5d7d9 0.5, #f4b096 0.65, #e36c56 0.85, #ac1727 1', // Set the color palette of the colorGradient. 
+                    // Set the color palette of the colorGradient. 
+                    colorGradient: 
+                    [
+                        '#3d51b8 0',
+                        '#6389ec 0.15',
+                        '#9fbffa 0.35',
+                        '#d5d7d9 0.5',
+                        '#f4b096 0.65',
+                        '#e36c56 0.85',
+                        '#ac1727 1'
+                    ].join(),
                     label: {
                         position: 'top'
                     }
