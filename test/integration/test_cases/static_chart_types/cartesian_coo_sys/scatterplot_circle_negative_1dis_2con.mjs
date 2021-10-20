7cf5111ab3f77038a1e47fcfc2245a39fff78bd9
andraskangyal <akangyal@gmail.com>
2021-10-20 15:22:43 +0200

    Tesxt case. Exists deleted.

--- test/integration/test_cases/static_chart_types/cartesian_coo_sys/scatterplot_circle_negative_1dis_2con.mjs
+++ test/integration/test_cases/static_chart_types/cartesian_coo_sys/scatterplot_circle_negative_1dis_2con.mjs
@@ -9,7 +9,7 @@ const testSteps = [
                 channels:
                 {
                     x: { attach: ['Value 6 (+/-)'] },
-                    y: { attach: ['$exists', 'Value 5 (+/-)'], range: { min: '0%', max: '110%' } },
+                    y: { attach: ['Value 5 (+/-)'], range: { min: '0%', max: '110%' } },
                     noop: { attach: ['Year'] },
                     label: { attach: ['Year'] }
                 },
