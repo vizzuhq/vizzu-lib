1cecfc92aa8a4954b1bc204d3e5079abbce866a9
andraskangyal <akangyal@gmail.com>
2021-11-09 16:47:23 +0100

    Test cases. Changed foldername.

---
+++ test/integration/test_cases/data_fault_and_formats/column_rectangle_less_disc.mjs
@@ -0,0 +1,20 @@
+import { data } from '/test/integration/test_data/data_missing_long.js';
+
+const testSteps = [
+  chart => chart.animate(
+    {
+      data: data,
+      config: {
+        channels: {
+          y: { attach: ['Channel title for long names', '値3'], range: { min: '110%', max: '0%' } },
+          x: { attach: ['Childs of long names which have no end'] },
+          color: { attach: ['Channel title for long names'] },
+          label: { attach: ['Childs of long names which have no end'] }
+        },
+        title: 'Data missing - Less Discrete'
+      }
+    }
+  )
+];
+
+export default testSteps;
