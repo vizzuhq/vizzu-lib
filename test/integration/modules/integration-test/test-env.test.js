527c4d59201db41a5d8a48375bc24e4b29cccdd1
David Vegh <david.andras.vegh+github@gmail.com>
2021-11-20 20:49:42 +0100

    integration test: fix naming convention

--- test/integration/modules/integration-test/test-env.test.js
+++ test/integration/modules/integration-test/test-env.test.js
@@ -2,18 +2,18 @@ const TestEnv = require("./test-env.js");
 const path = require("path");
 
 
-test("if getWorkspacePath() is valid", () => {
+test("if getWorkspacePath() returns workspacePath", () => {
     expect(TestEnv.getWorkspacePath()).toBe(path.join(process.cwd(), "../.."));
 });
 
-test("if getTestSuitePath() is valid", () => {
+test("if getTestSuitePath() returns testSuitePath", () => {
     expect(TestEnv.getTestSuitePath()).toBe(process.cwd());
 });
 
-test("if getTestSuiteReportPath() is valid", () => {
+test("if getTestSuiteReportPath() returns testSuiteReportPath", () => {
     expect(TestEnv.getTestSuiteReportPath()).toBe(path.join(process.cwd(), "test_report"));
 });
 
-test("if getTestSuiteResultsPath() is valid", () => {
+test("if getTestSuiteResultsPath() returns testSuiteResultsPath", () => {
     expect(TestEnv.getTestSuiteResultsPath()).toBe(path.join(process.cwd(), "test_report", "results"));
 });
