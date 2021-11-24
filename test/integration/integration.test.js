e29d497b5c40549b6dad46ee927ba6a3f00dcc8b
David Vegh <david.andras.vegh+github@gmail.com>
2021-11-24 11:33:06 +0100

    integartion test: separate files

--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -10,8 +10,8 @@ const catchError = (err => {
     if (err.stack !== undefined) {
         errMsg = err.stack;
     }
-    if (testConsole) {
-        testConsole.log("[ " + "ERROR".padEnd(testConsole.testStatusPad, " ") + " ] " + errMsg);
+    if (cnsl) {
+        cnsl.log("[ " + "ERROR".padEnd(cnsl.testStatusPad, " ") + " ] " + errMsg);
     } else {
         console.log("[ ERROR ] " + errMsg);
     }
@@ -21,13 +21,13 @@ const catchError = (err => {
 try {
     var argv = yargs
 
-        .usage("Usage: $0 [tests] [options]" + 
-                    "\n\nThe objective of the integration test is to E2E test the Vizzu library." + 
-                    "\nThe test cases are animations, each animation consists of an animate function call promise chain." + 
-                    "\nThe test animations run in Chrome using ChromeDriver and Selenium Webdriver." + 
-                    "\nA test case seeks through each animate function calls with a predefined animation step." +
-                    "\nA hash is calculated on every created canvas image data for each animation step." + 
-                    "\nA test validates a hash calculated from the created hash list during each test case.")
+        .usage("Usage: $0 [tests] [options]" +
+            "\n\nThe objective of the integration test is to E2E test the Vizzu library." +
+            "\nThe test cases are animations, each animation consists of an animate function call promise chain." +
+            "\nThe test animations run in Chrome using ChromeDriver and Selenium Webdriver." +
+            "\nA test case seeks through each animate function calls with a predefined animation step." +
+            "\nA hash is calculated on every created canvas image data for each animation step." +
+            "\nA test validates a hash calculated from the created hash list during each test case.")
 
         .help("h")
         .alias("h", "help")
@@ -35,127 +35,119 @@ try {
         .version("0.1.0")
         .alias("v", "version")
 
-        .string("ref")
-        .nargs("ref", 1)
-        .describe("ref", 
-                    "Change reference hash list file path of test cases" + 
-                    "\n(relative or absolute path where the repo folder is the root)" + 
-                    "\n")
-        .default("ref", 
-                    "/test/integration/test_cases/ref_list.json")
+        .array("c")
+        .alias("c", "configs")
+        .nargs("c", 1)
+        .describe("c",
+            "Change the list of config file's path of the test cases" +
+            "\n(relative or absolute path where the repo folder is the root)" +
+            "\n")
+        .default("c",
+            ["/test/integration/test_cases/test_cases.json"])
 
-        .string("cases")
-        .nargs("cases", 1)
-        .describe("cases", 
-                    "Change folder path of test cases" + 
-                    "\n(relative or absolute path where the repo folder is the root)" + 
-                    "\n")
-        .default("cases", 
-                    "/test/integration/test_cases")
-
-        .choices("images", 
-                    ["ALL", "FAILED", "DISABLED"])
-        .describe("images", 
-                    "Change report images saving behavior" + 
-                    "\n- \"ALL\": Create images for every test" + 
-                    "\n- \"FAILED\": Create images for failed/warning tests only" + 
-                    "\n- \"DISABLED\": Do not create images")
+        .choices("images",
+            ["ALL", "FAILED", "DISABLED"])
+        .describe("images",
+            "Change report images saving behavior" +
+            "\n- \"ALL\": Create images for every test" +
+            "\n- \"FAILED\": Create images for failed/warning tests only" +
+            "\n- \"DISABLED\": Do not create images")
         .default("images", "FAILED")
 
-        .choices("hashes", 
-                    ["ALL", "FAILED", "DISABLED"])
-        .describe("hashes", 
-                    "Change report hashes saving behavior" + 
-                    "\n- \"ALL\": Write hashes into the report file for every test" + 
-                    "\n- \"FAILED\": Write hashes into the report file for failed/warning tests only" + 
-                    "\n- \"DISABLED\": Do not create report file")
+        .choices("hashes",
+            ["ALL", "FAILED", "DISABLED"])
+        .describe("hashes",
+            "Change report hashes saving behavior" +
+            "\n- \"ALL\": Write hashes into the report file for every test" +
+            "\n- \"FAILED\": Write hashes into the report file for failed/warning tests only" +
+            "\n- \"DISABLED\": Do not create report file")
         .default("hashes", "FAILED")
 
         .boolean("nologs")
-        .describe("nologs", 
-                    "\n Do not save browser and console log into file" + 
-                    "\n")
+        .describe("nologs",
+            "\n Do not save browser and console log into file" +
+            "\n")
         .default("nologs", false)
 
         .string("vizzu")
         .nargs("vizzu", 1)
-        .describe("vizzu", 
-                    "Change Vizzu url" + 
-                    "\n(can be forced to use vizzu.js or vizzu.min.js if its given)" + 
-                    "\n\n- \"head\": select the last stable Vizzu from the main branch" + 
-                    "\n(default: vizzu.min.js)" + 
-                    "\n\n- [sha]: select Vizzu with a short commit number" + 
-                    "\n(default: vizzu.min.js)" + 
-                    "\n\n- [version]: select Vizzu with a version number" + 
-                    "\n(vizzu.min.js only)" + 
-                    "\n\n- path: select Vizzu from the local file system" + 
-                    "\n(relative or absolute path where the repo folder is the root)" +  
-                    "\n(default: vizzu.js)" + 
-                    "\n")
-        .default("vizzu", 
-                    "/example/lib/vizzu.js")
+        .describe("vizzu",
+            "Change Vizzu url" +
+            "\n(can be forced to use vizzu.js or vizzu.min.js if its given)" +
+            "\n\n- \"head\": select the last stable Vizzu from the main branch" +
+            "\n(default: vizzu.min.js)" +
+            "\n\n- [sha]: select Vizzu with a short commit number" +
+            "\n(default: vizzu.min.js)" +
+            "\n\n- [version]: select Vizzu with a version number" +
+            "\n(vizzu.min.js only)" +
+            "\n\n- path: select Vizzu from the local file system" +
+            "\n(relative or absolute path where the repo folder is the root)" +
+            "\n(default: vizzu.js)" +
+            "\n")
+        .default("vizzu",
+            "/example/lib/vizzu.js")
 
         .boolean("g")
         .alias("g", "gui")
-        .describe("g", 
-                    "Use browser with graphical user interface" + 
-                    "\n")
+        .describe("g",
+            "Use browser with graphical user interface" +
+            "\n")
         .default("g", false)
 
         .number("b")
         .alias("b", "browsers")
-        .describe("b", 
-                    "Change number of parallel browser windows" + 
-                    "\n")
-        .default("b", 4)
+        .describe("b",
+            "Change number of parallel browser windows" +
+            "\n")
+        .default("b", 3)
 
 
-        .boolean("del")
-        .alias("del", "delete")
-        .describe("del", 
-                    "Delete test report folder" + 
-                    "\n")
-        .default("del", false)
+        .boolean("d")
+        .alias("d", "delete")
+        .describe("d",
+            "Delete test report folder" +
+            "\n")
+        .default("d", false)
 
         .example([
-                    ["$0", 
-                        "Run all tests from the test cases folder"],
-                    ["$0 area_carte_2_polar.mjs", 
-                        "Select test case with name"],
-                    ["$0 area_carte_2_polar", 
-                        "Select test case with name (without extension)"],
-                    ["$0 area_carte_2_polar rectangle_carte_2_polar", 
-                        "Select test cases with name"],
-                    ["$0 basic_animations/coordsystems/area_carte_2_polar.mjs", 
-                        "Select test case with path"],
-                    ["$0 ./test_cases/basic_animations/coordsystems/*", 
-                        "Select test cases with glob pattern"]
-                ])
-        .example("$0 -g", 
-                    "Run all tests and use browser with graphical user interface")
+            ["$0",
+                "Run all tests"],
+            ["$0 area_carte_2_polar.mjs",
+                "Select test case with name"],
+            ["$0 area_carte_2_polar",
+                "Select test case with name (without extension)"],
+            ["$0 area_carte_2_polar rectangle_carte_2_polar",
+                "Select test cases with name"],
+            ["$0 basic_animations/coordsystems/area_carte_2_polar.mjs",
+                "Select test case with path"],
+            ["$0 ./test_cases/basic_animations/coordsystems/*",
+                "Select test cases with glob pattern"]
+        ])
+        .example("$0 -g",
+            "Run all tests and use browser with graphical user interface")
         .example([
-                    ["$0 -vizzu head", 
-                        "Run all tests with the latest stable Vizzu from the main branch"],
-                    ["$0 -vizzu [sha]/vizzu.js", 
-                        "Run all tests and select Vizzu with a short commit number" + 
-                        "\nand use vizzu.js instead of the default vizzu.min.js"]
-                ])
+            ["$0 -vizzu head",
+                "Run all tests with the latest stable Vizzu from the main branch"],
+            ["$0 -vizzu [sha]/vizzu.js",
+                "Run all tests and select Vizzu with a short commit number" +
+                "\nand use vizzu.js instead of the default vizzu.min.js"]
+        ])
 
         .argv;
 
-    if (!argv.del) {
-        let testSuite = new TestSuite(  
-                                        argv.cases, 
-                                        argv.ref, 
-                                        argv._, 
-                                        argv.vizzu, 
-                                        argv.gui, 
-                                        argv.browsers, 
-                                        argv.nologs,
-                                        argv.images, 
-                                        argv.hashes
-                                    );
-        var testConsole = testSuite.cnsl();
+    if (!argv.delete) {
+        let testSuite = new TestSuite(
+            argv.configs,
+            argv._,
+            argv.nologs,
+            argv.browsers,
+            argv.gui,
+            argv.vizzu,
+            argv.gui,
+            argv.images,
+            argv.hashes
+        );
+        var cnsl = testSuite.cnsl();
         testSuite.test().catch(err => {
             catchError(err);
         });
---
+++ test/integration/modules/integration-test/test-suite-result.js
@@ -0,0 +1,98 @@
+
+class TestSuiteResult {
+
+    #cnsl;
+
+    #testSuiteResults;
+    #testCases;
+    #testCasesConfig;
+    #createHashes;
+
+
+    constructor(
+        testSuiteResults,
+        testCases,
+        testCasesConfig,
+        createHashes,
+        cnsl
+    ) {
+
+        this.#cnsl = cnsl;
+
+        this.#testSuiteResults = testSuiteResults;
+        this.#testCases = testCases;
+        this.#testCasesConfig = testCasesConfig;
+        this.#createHashes = createHashes;
+    }
+
+
+    createTestSuiteResult() {
+        //this.#createNewConfig();
+        this.#testSuiteResults.TIME.END = Math.round(Date.now() / 1000);
+        let duration = this.#testSuiteResults.TIME.END - this.#testSuiteResults.TIME.START;
+        this.#cnsl.log("\n" + "duration:".padEnd(15, " ") + duration + "s");
+        this.#cnsl.log("\n" + "all tests:".padEnd(15, " ") + this.#testCases.testCases.length);
+        const sum = this.#testSuiteResults.PASSED.length + this.#testSuiteResults.WARNING.length + this.#testSuiteResults.FAILED.length;
+        this.#cnsl.log("tests run:".padEnd(15, " ") + sum);
+        this.#cnsl.log(("tests passed:".padEnd(15, " ") + this.#testSuiteResults.PASSED.length).success);
+        if (this.#testSuiteResults.WARNING.length != 0) {
+            this.#cnsl.log(("tests warning:".padEnd(15, " ") + this.#testSuiteResults.WARNING.length).warn);
+        } else {
+            this.#cnsl.log("tests warning:".padEnd(15, " ") + this.#testSuiteResults.WARNING.length);
+        }
+        if (this.#testSuiteResults.FAILED.length != 0) {
+            this.#cnsl.log(("tests failed:".padEnd(15, " ") + this.#testSuiteResults.FAILED.length).error);
+            process.exitCode = 1;
+            this.#testSuiteResults.FAILED.forEach(testCase => {
+                this.#cnsl.log("".padEnd(this.#cnsl.getTestStatusPad() + 5, " ") + testCase + " http://127.0.0.1:8080/test/integration/manual/?version=localhost&testCase=" + testCase);
+            });
+        } else {
+            this.#cnsl.log("tests failed:".padEnd(15, " ") + this.#testSuiteResults.FAILED.length);
+        }
+    }
+
+
+    /*#createNewConfig() {
+        if (this.#createHashes !== "DISABLED") {
+            if (this.#createHashes === "ALL" || (this.#createHashes === "FAILED" && (this.#testSuiteResults.FAILED.length !== 0 || this.#testSuiteResults.WARNING !== 0))) {
+                let hashesPath = path.join(TestEnv.getTestSuiteResultsPath(), path.basename(this.#testCasesHashListPath));
+                let rmReady = new Promise((resolve, reject) => {
+                    fs.rm(hashesPath, { force: true }, err => {
+                        if (err) {
+                            return reject(err);
+                        }
+                        return resolve();
+                    });
+                });
+                let mkdirReady = new Promise((resolve, reject) => {
+                    fs.mkdir(TestEnv.getTestSuiteResultsPath(), { recursive: true }, err => {
+                        if (err) {
+                            return reject(err);
+                        }
+                        return resolve();
+                    });
+                });
+                Promise.all([rmReady, mkdirReady]).then(() => {
+                    let testCasesData = {};
+                    for (const [key, value] of Object.entries(this.#testSuiteResults.RESULTS)) {
+                        if (this.#cfgCreateHashes === "FAILED" && (!this.#testSuiteResults.FAILED.includes(key) && !this.#testSuiteResults.WARNING.includes(key))) {
+                            continue;
+                        }
+                        testCasesData[key] = { refs: [this.#testSuiteResults.RESULTS[key]['hash']] };
+                    }
+                    if (Object.keys(testCasesData).length !== 0) {
+                        testCasesData = JSON.stringify(testCasesData, null, 4);
+                        fs.writeFile(hashesPath, testCasesData, (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    }*/
+}
+
+
+module.exports = TestSuiteResult;
