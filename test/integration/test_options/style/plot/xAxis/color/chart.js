e2523fae1d80bf2f496785d25d2a9eeebae7a9b5
David Vegh <david.andras.vegh+github@gmail.com>
2021-12-12 15:23:31 +0100

    integration test: add option tests and reduce color tests

--- test/integration/test_options/style/plot/xAxis/color/chart.js
+++ test/integration/test_options/style/plot/xAxis/color/chart.js
@@ -4,7 +4,7 @@ import { data } from "/test/integration/test_options/data.js";
 export default class Chart {
     static static(color) {
         return chart => {
-            console.log("plot borderColor: " + color);
+            console.log("plot xAxis color: " + color);
             return chart.animate(
             {
                 data: data,
@@ -18,8 +18,9 @@ export default class Chart {
                 },
                 style: {
                     plot: { 
-                        borderWidth: 5,
-                        borderColor: color 
+                        xAxis: {
+                            color: color
+                        } 
                     }
                 }
             });
@@ -28,13 +29,14 @@ export default class Chart {
 
     static animated(color) {
         return chart => {
-            console.log("plot borderColor: " + color);
+            console.log("plot xAxis color: " + color);
             return chart.animate(
             {
                 style: {
                     plot: { 
-                        borderWidth: 5,
-                        borderColor: color 
+                        xAxis: {
+                            color: color
+                        } 
                     }
                 }
             })
