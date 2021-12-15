6e2b5e6b1453f5ecdbe985958895a630598e17fc
David Vegh <david.andras.vegh+github@gmail.com>
2021-12-15 11:37:25 +0100

    integration test: add option tests

---
+++ test/integration/test_options/style/plot/yAxis/ticks/length/static/element_font_percentage/2.mjs
@@ -0,0 +1,9 @@
+import Chart from "/test/integration/test_options/style/plot/yAxis/ticks/length/chart.js";
+
+
+const testSteps = [
+    Chart.static(new URL(import.meta.url), 'em')
+]
+
+
+export default testSteps
