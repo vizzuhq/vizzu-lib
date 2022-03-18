d1c98071b4b7edf53484ecd365e7d532a9c3cea0
Laszlo Simon <simon@vizzuhq.com>
2022-03-18 16:30:30 +0100

    Version number change 0.4.6 -> 0.4.7

--- src/chart/main/version.cpp
+++ src/chart/main/version.cpp
@@ -1,5 +1,5 @@
 #include "version.h"
 
-const App::Version Vizzu::Main::version(0, 4, 6);
+const App::Version Vizzu::Main::version(0, 4, 7);
 
 const char *Vizzu::Main::siteUrl = "https://lib.vizzuhq.com/";
