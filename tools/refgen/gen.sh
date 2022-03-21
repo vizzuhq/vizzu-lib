7cbd7bb0cbbd7a321673d9e06ed8546345fbbf2a
Laszlo Simon <simon@vizzuhq.com>
2022-03-21 14:11:54 +0100

    Docs: reference generator moved to vizzu-lib-doc repository.

--- tools/refgen/gen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# works with: typedoc 0.22.3, typedoc-plugin-markdown: 3.11.0  
-npx typedoc --readme ../../docs/content/reference/reference.md --out ../../docs/reference ../../src/apps/weblib/js-api/vizzu.d.ts
-# --hideBreadcrumbs true --hideInPageTOC true
