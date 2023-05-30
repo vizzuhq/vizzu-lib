c18d002f0d483ae9eed4a566689807bae67589eb
Laszlo Simon <simon@vizzuhq.com>
2023-05-30 15:14:32 +0200

    Object registry class moved to separate header file.

--- src/apps/weblib/interface.h
+++ src/apps/weblib/interface.h
@@ -1,44 +1,13 @@
 #ifndef LIB_INTERFACE_H
 #define LIB_INTERFACE_H
 
-#include <unordered_map>
-
 #include "chart/main/version.h"
 #include "chart/ui/chart.h"
 
+#include "objectregistry.h"
+
 namespace Vizzu
 {
-
-class ObjectRegistry
-{
-public:
-	typedef void* Handle;
-
-	Handle reg(std::shared_ptr<void> ptr) {
-		Handle handle = ptr.get();
-		objects.emplace(handle, ptr);
-		return handle;
-	}
-
-	template <class T>
-	std::shared_ptr<T> get(Handle handle)
-	{
-		auto it = objects.find(handle);
-		if (it == objects.end() || !it->second) 
-			throw std::logic_error("No such object exists");
-		return std::static_pointer_cast<T>(it->second);
-	}
-
-	void unreg(Handle handle)
-	{
-		auto it = objects.find(handle);
-		if (it == objects.end()) throw std::logic_error("No such object exists");
-		objects.erase(it);
-	}
-
-private:
-	std::unordered_map<void*, std::shared_ptr<void>> objects;
-};
 
 class Interface
 {
