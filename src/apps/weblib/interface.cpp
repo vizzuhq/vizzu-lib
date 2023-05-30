1b41232a7864ba8b410adca3e392e4c6b0d80737
Laszlo Simon <simon@vizzuhq.com>
2023-05-30 15:12:04 +0200

    JS API extern declarations moved to separate header file.

--- src/apps/weblib/interface.cpp
+++ src/apps/weblib/interface.cpp
@@ -8,13 +8,7 @@
 #include "base/text/jsonoutput.h"
 #include "jscriptcanvas.h"
 
-extern "C" {
-	extern void jsconsolelog(const char*);
-	extern void openUrl(const char*);
-	extern void setCursor(const char *cursor);
-	extern void event_invoked(int, const char*);
-	extern void removeJsFunction(void *);
-}
+#include "interfacejs.h"
 
 using namespace Util;
 using namespace Vizzu;
---
+++ src/apps/weblib/interfacejs.h
@@ -0,0 +1,12 @@
+#ifndef INTERFACEJS_H
+#define INTERFACEJS_H
+
+extern "C" {
+	extern void jsconsolelog(const char*);
+	extern void openUrl(const char*);
+	extern void setCursor(const char *cursor);
+	extern void event_invoked(int, const char*);
+	extern void removeJsFunction(void *);
+}
+
+#endif
