d2f9e4764b283fe2dede9716c9717db3622d2206
Bela Schaum <schaumb@gmail.com>
2023-06-16 18:53:46 +0200

    Remove other const-cast

--- src/chart/options/optionssetter.cpp
+++ src/chart/options/optionssetter.cpp
@@ -177,7 +177,7 @@ OptionsSetter &OptionsSetter::setStackable(const ChannelId &channelId,
 {
 	auto &channel = options.getChannels().at(channelId);
 	if (channel.stackable() != value) {
-		const_cast<bool &>(channel.stackable()) = value;
+		channel.setStackable(value);
 		changed = true;
 	}
 	return *this;
