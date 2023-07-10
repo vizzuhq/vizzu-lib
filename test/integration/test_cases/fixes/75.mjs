b4f9ad9cfbca72c95e6909668467ea371ae40352
Bela Schaum <schaumb@gmail.com>
2023-07-10 14:26:02 +0200

    Tab/space in testcase

--- test/integration/test_cases/fixes/75.mjs
+++ test/integration/test_cases/fixes/75.mjs
@@ -18,16 +18,16 @@ const testSteps = [
     return chart.animate({ data });
   },
   (chart) => {
-	chart.on('plot-axis-label-draw', eh);
-	chart.on('plot-marker-label-draw', eh);
+    chart.on('plot-axis-label-draw', eh);
+    chart.on('plot-marker-label-draw', eh);
     return chart.animate({
       x: "Foo",
       y: "Bar",
     });
   },
   (chart) => {
-	chart.off('plot-axis-label-draw', eh);
-	chart.off('plot-marker-label-draw', eh);
+    chart.off('plot-axis-label-draw', eh);
+	  chart.off('plot-marker-label-draw', eh);
     return chart.animate({
       x: "Foo",
       y: "Bar",
