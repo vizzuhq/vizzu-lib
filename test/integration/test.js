79e54b3e37158b76343abfa02a9acb4aa2ed82a2
David Vegh <david.andras.vegh@gmail.com>
2023-07-14 17:22:59 +0200

    Integration test, add geometry tests

--- test/integration/test.js
+++ test/integration/test.js
@@ -59,6 +59,7 @@ Please note that the test require Chrome, ChromeDriver and Selenium Webdriver to
     )
     .default("c", [
       "/test/integration/test_cases/test_cases.json",
+      "/test/integration/tests/config_tests.json",
       "/test/integration/tests/style_tests.json",
       "/test/integration/tests/features.json",
       "/test/integration/tests/fixes.json",
