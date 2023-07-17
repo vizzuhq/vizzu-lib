19d922764c6df35d416aa8152e52ce80440e2988
Bela Schaum <schaumb@gmail.com>
2023-07-17 11:02:46 +0200

    Fix tests + modify accept changes

--- test/integration/accept-changes.js
+++ test/integration/accept-changes.js
@@ -1,28 +1,47 @@
 const fs = require("fs");
 
 function copyHashes(failHashFile, refHashFile) {
-  const failHashData = JSON.parse(fs.readFileSync(failHashFile));
-  const refHashData = JSON.parse(fs.readFileSync(refHashFile));
+  try {
+    const failHashData = JSON.parse(fs.readFileSync(failHashFile));
+    const refHashData = JSON.parse(fs.readFileSync(refHashFile));
 
-  for (const testFilename in failHashData.test) {
-    if (!refHashData.test[testFilename]) {
-      refHashData.test[testFilename] = {
-        refs: [failHashData.test[testFilename].refs[0]],
-      };
-    } else if (refHashData.test[testFilename].refs[0] !== "")
-      refHashData.test[testFilename].refs[0] =
-        failHashData.test[testFilename].refs[0];
+    for (const testFilename in failHashData.test) {
+      if (!refHashData.test[testFilename]) {
+        refHashData.test[testFilename] = {
+          refs: [failHashData.test[testFilename].refs[0]],
+        };
+      } else if (refHashData.test[testFilename].refs[0] !== "")
+        refHashData.test[testFilename].refs[0] =
+          failHashData.test[testFilename].refs[0];
+    }
+
+    fs.writeFileSync(refHashFile, JSON.stringify(refHashData, null, 4));
+  } catch (error) {
+    // console.info(error);
   }
-
-  fs.writeFileSync(refHashFile, JSON.stringify(refHashData, null, 4));
 }
 
 copyHashes(
   "test_report/results/test_cases/test_cases.json",
   "test_cases/test_cases.json"
 );
-/*
+
 copyHashes(
-	"test_report/results/tests/style/style_tests.json",
-	"tests/style_tests.json"
-)*/
+  "test_report/results/tests/style_tests/style_tests.json",
+  "tests/style_tests.json"
+);
+
+copyHashes(
+  "test_report/results/tests/features/features.json",
+  "tests/features.json"
+);
+
+copyHashes(
+  "test_report/results/tests/config_tests/config_tests.json",
+  "tests/config_tests.json"
+);
+
+copyHashes(
+  "test_report/results/tests/fixes/fixes.json",
+  "tests/fixes.json"
+);
