549924339d5badc3e48f1ffeeb8721a1949a10ab
Laszlo Simon <simon@vizzuhq.com>
2023-07-20 15:04:13 +0200

    Axis demo in tutorial formatted.

--- docs/tutorial/axis-demo/axis-demo.js
+++ docs/tutorial/axis-demo/axis-demo.js
@@ -1,90 +1,99 @@
-import Vizzu from 'https://vizzu-lib-main.storage.googleapis.com/lib/vizzu.js';
+import Vizzu from "https://vizzu-lib-main.storage.googleapis.com/lib/vizzu.js";
 
-let animopts = {
-	x: { delay: 0, duration: .7 },
-	y: { delay: 0, duration: .7 },
-	style: { delay: 0, duration: .7 },
-}
-
-let chart = new Vizzu("axis-demo", {
-	data: {
-		series: [
-			{ name: 'Dimension', values: ['Alice', 'Bob', 'Ted'] },
-			{ name: 'Measure', values: [4, 3, 2] }
-		]
-	},
-	config: {
-		legend: null,
-		x: { step: 1, range: { max: '100%' } }
-	}, style: {
-		fontSize: "1em",
-		plot: { marker: { rectangleSpacing: 0 } },
-		paddingBottom: 0,
-		paddingTop: 0,
-		paddingLeft: 0,
-		paddingRight: 0
-	}
-});
-
-chart.initializing.then(chart => {
-	chart.on('logo-draw', e => e.preventDefault());
-	return chart;
-})
-
-let f0 = () => {
-	chart.animate({
-		config: {
-			x: 'Dimension',
-			label: 'Dimension',
-			color: 'Dimension',
-			orientation: 'vertical'
-		},
-		style: {
-			"plot.marker.label": {
-				filter: 'lightness(0.7)',
-				paddingLeft: null
-			}
-		}
-	}, animopts)
+const animopts = {
+  x: { delay: 0, duration: 0.7 },
+  y: { delay: 0, duration: 0.7 },
+  style: { delay: 0, duration: 0.7 },
 };
 
+const chart = new Vizzu("axis-demo", {
+  data: {
+    series: [
+      { name: "Dimension", values: ["Alice", "Bob", "Ted"] },
+      { name: "Measure", values: [4, 3, 2] },
+    ],
+  },
+  config: {
+    legend: null,
+    x: { step: 1, range: { max: "100%" } },
+  },
+  style: {
+    fontSize: "1em",
+    plot: { marker: { rectangleSpacing: 0 } },
+    paddingBottom: 0,
+    paddingTop: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+});
 
-let f1 = () => {
-	chart.animate({
-		config: {
-			x: 'Measure',
-			color: 'Dimension',
-			label: 'Dimension',
-			orientation: 'vertical'
-		},
-		style: {
-			"plot.marker.label": {
-				filter: 'lightness(0.7)',
-				paddingLeft: '-2.4em'
-			}
-		}
-	}, animopts);
-}
+chart.initializing.then((chart) => {
+  chart.on("logo-draw", (e) => e.preventDefault());
+  return chart;
+});
 
-let f2 = () => {
-	chart.animate({
-		config: {
-			x: ['Measure', 'Dimension'],
-			color: 'Dimension',
-			label: 'Dimension',
-			orientation: 'vertical'
-		},
-		style: {
-			"plot.marker.label": {
-				filter: 'lightness(0.7)',
-				paddingLeft: null
-			}
-		}
-	}, animopts);
-}
+const f0 = () => {
+  chart.animate(
+    {
+      config: {
+        x: "Dimension",
+        label: "Dimension",
+        color: "Dimension",
+        orientation: "vertical",
+      },
+      style: {
+        "plot.marker.label": {
+          filter: "lightness(0.7)",
+          paddingLeft: null,
+        },
+      },
+    },
+    animopts
+  );
+};
+
+const f1 = () => {
+  chart.animate(
+    {
+      config: {
+        x: "Measure",
+        color: "Dimension",
+        label: "Dimension",
+        orientation: "vertical",
+      },
+      style: {
+        "plot.marker.label": {
+          filter: "lightness(0.7)",
+          paddingLeft: "-2.4em",
+        },
+      },
+    },
+    animopts
+  );
+};
+
+const f2 = () => {
+  chart.animate(
+    {
+      config: {
+        x: ["Measure", "Dimension"],
+        color: "Dimension",
+        label: "Dimension",
+        orientation: "vertical",
+      },
+      style: {
+        "plot.marker.label": {
+          filter: "lightness(0.7)",
+          paddingLeft: null,
+        },
+      },
+    },
+    animopts
+  );
+};
 
 f0();
 
-document.getElementById('axis-demo-0').onclick = f0;
-document.getElementById('axis-demo-1').onclick = f1;
-document.getElementById('axis-demo-2').onclick = f2;
+document.getElementById("axis-demo-0").onclick = f0;
+document.getElementById("axis-demo-1").onclick = f1;
+document.getElementById("axis-demo-2").onclick = f2;
--- docs/tutorial/axis-demo/axis-demo.md
+++ docs/tutorial/axis-demo/axis-demo.md
@@ -1,18 +1,31 @@
 ```javascript
 let data = {
-  series: [
-    { name: 'Dimension', values: ['Alice', 'Bob', 'Ted'] },
-    { name: 'Measure', values: [4, 3, 2] }
-  ]
+    series: [{
+        name: 'Dimension',
+        values: ['Alice', 'Bob', 'Ted']
+    }, {
+        name: 'Measure',
+        values: [4, 3, 2]
+    }]
 };
 ```
+
 ```javascript id="axis-demo-0"
-chart.animate({ x: Discrete })
+chart.animate({
+    x: Discrete
+})
 ```
+
 ```javascript id="axis-demo-1"
-chart.animate({ x: Measure })
+chart.animate({
+    x: Measure
+})
 ```
+
 ```javascript id="axis-demo-2"
-chart.animate({ x: [Measure, Discrete] })
+chart.animate({
+    x: [Measure, Discrete]
+})
 ```
+
 <div id="axis-demo" style="width:500px; height:125px"></div>
