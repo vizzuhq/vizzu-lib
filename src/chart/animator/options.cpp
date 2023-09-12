32a4317e2ec0f84237d229731284818734cfb407
Laszlo Simon <simon@vizzuhq.com>
2023-09-12 13:43:30 +0200

    Default regroup strategy change to drilldown for debug purposes

--- src/chart/animator/options.cpp
+++ src/chart/animator/options.cpp
@@ -72,5 +72,5 @@ const Options::Section &Options::Keyframe::get(
 RegroupStrategy Options::Keyframe::getRegroupStrategy() const
 {
 	return regroupStrategy ? *regroupStrategy
-	                       : RegroupStrategy::aggregate;
+	                       : RegroupStrategy::drilldown;
 }
