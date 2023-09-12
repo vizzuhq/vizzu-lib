64609a7bedc1c7a0e0bd9e273e350ca3a2842a59
Laszlo Simon <simon@vizzuhq.com>
2023-09-12 16:28:48 +0200

    Fixed axis range for filtered out markers.

---
+++ test/integration/tests/fixes/32303048.mjs
@@ -0,0 +1,22 @@
+const testSteps = [
+	chart => {
+		let data = {
+			series: [
+				{ name: 'Foo', values: ['Alice', 'Bob', 'Ted'] },
+				{ name: 'Bar', values: [15, 32, 12] }
+			]
+		};
+		return chart.animate({ data }, 0);
+	},
+	chart => {
+		return chart.animate({
+			data: { filter: record => record.Foo == 'Alice' },
+			config: {
+				x: 'Foo',
+				y: ['Foo', 'Bar']
+			}
+		}, 0);
+	}
+];
+
+export default testSteps;
