beae89295944642a56b14fb93373a17138c260fd
David Vegh <veghdev@gmail.com>
2023-10-03 11:29:00 +0200

    Able to download sha lib

--- tools/ci/run/pkg-rollup-jsonly.sh
+++ tools/ci/run/pkg-rollup-jsonly.sh
@@ -7,7 +7,12 @@ mkdir -p dist
 
 cp src/apps/weblib/js-api/* dist/
 
-wget 'https://vizzu-lib-main.storage.googleapis.com/lib/cvizzu.js' -O dist/cvizzu.js
-wget 'https://vizzu-lib-main.storage.googleapis.com/lib/cvizzu.wasm' -O dist/cvizzu.wasm
+LIB_URL="https://vizzu-lib-main.storage.googleapis.com/lib"
+if [ "$1" ]; then
+  LIB_URL="https://vizzu-lib-main-sha.storage.googleapis.com/lib-$1"
+fi
+
+wget "$LIB_URL/cvizzu.js" -O dist/cvizzu.js
+wget "$LIB_URL/cvizzu.wasm" -O dist/cvizzu.wasm
 
 npm run pkg-rollup-js
