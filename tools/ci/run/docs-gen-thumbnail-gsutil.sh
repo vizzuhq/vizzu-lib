fc7a33c480e063d8e42f107957ab2605ae564d92
David Vegh <veghdev@gmail.com>
2023-10-06 17:33:36 +0200

    Update contributing

--- tools/ci/run/docs-gen-thumbnail-gsutil.sh
+++ tools/ci/run/docs-gen-thumbnail-gsutil.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-source .venv/bin/activate
-
 hash=$(node ./tools/docs/examples/gen_webcontent_hash.cjs)
 echo $hash
 
@@ -12,7 +10,7 @@ if gsutil -q stat "gs://vizzu-lib-thumbnails/$hash/exists.txt"; then
     gsutil -m cp -r "gs://vizzu-lib-thumbnails/$hash/presets" /workspace/docs/examples/
     gsutil -m cp -r "gs://vizzu-lib-thumbnails/$hash/static" /workspace/docs/examples/
 else
-    python tools/docs/examples/gen_thumbnails.py
+    ./tools/ci/run/docs-gen-thumbnail.sh
     gsutil -m cp -r /workspace/docs/examples/analytical_operations "gs://vizzu-lib-thumbnails/$hash/"
     gsutil -m cp -r /workspace/docs/examples/presets "gs://vizzu-lib-thumbnails/$hash/"
     gsutil -m cp -r /workspace/docs/examples/static "gs://vizzu-lib-thumbnails/$hash/"
