59f14425ebea8ca01a3a3396a1e125627960498a
David Vegh <veghdev@gmail.com>
2023-10-24 12:20:28 +0200

    Set max failed images

--- tools/ci/run/test-gsutil.sh
+++ tools/ci/run/test-gsutil.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm test -- --vizzu /dist/vizzu.min.js --hashes ALL || TEST_FAILED=true
+npm test -- --vizzu /dist/vizzu.min.js --hashes ALL --maxFailedImages 100 || TEST_FAILED=true
 tar -czvf test/integration/test_report.tgz test/integration/test_report/ || TGZ_FAILED=true
 gsutil cp 'test/integration/test_report.tgz' 'gs://vizzu-lib-main-sha/lib-$SHORT_SHA/test_report.tgz' || GSUTIL_FAILED=true
 if [ "$TEST_FAILED" == "true" ] || [ "$TGZ_FAILED" == "true" ] || [ "$GSUTIL_FAILED" == "true" ]; then
