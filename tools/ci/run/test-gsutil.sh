e2ff7129bf88758a6d4331ecec5e4a4add3d5984
David Vegh <veghdev@gmail.com>
2023-10-24 12:16:52 +0200

    Place gsutil test into sh script

---
+++ tools/ci/run/test-gsutil.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+npm test -- --vizzu /dist/vizzu.min.js --hashes ALL || TEST_FAILED=true
+tar -czvf test/integration/test_report.tgz test/integration/test_report/ || TGZ_FAILED=true
+gsutil cp 'test/integration/test_report.tgz' 'gs://vizzu-lib-main-sha/lib-$SHORT_SHA/test_report.tgz' || GSUTIL_FAILED=true
+if [ "$TEST_FAILED" == "true" ] || [ "$TGZ_FAILED" == "true" ] || [ "$GSUTIL_FAILED" == "true" ]; then
+    exit 1
+fi
