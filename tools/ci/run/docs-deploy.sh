6d8aa08a3d19d8a8e3d3c5809b9a59c4db17a4ca
David Vegh <veghdev@gmail.com>
2023-11-10 10:15:15 +0100

    Fix docs deploy

--- tools/ci/run/docs-deploy.sh
+++ tools/ci/run/docs-deploy.sh
@@ -2,5 +2,15 @@
 
 set -e
 
+rm -rf docs/assets/dist
+mkdir -p docs/assets/dist
+cp dist/vizzu.min.js docs/assets/dist/vizzu.min.js
+cp dist/cvizzu.wasm docs/assets/dist/cvizzu.wasm
+
+rm -rf docs/showcases
+cp -r vizzu-lib-doc/docs/showcases docs/
+rm -rf docs/readme
+cp -r vizzu-lib-doc/docs/readme docs/
+
 source .venv/bin/activate
 python tools/docs/deploy.py
