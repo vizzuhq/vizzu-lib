20b3c09c47c80715a05f9973cd31bf2f4b47d282
David Vegh <veghdev@gmail.com>
2023-12-18 15:56:42 +0100

    Fixed missing cvizzu.d.ts and cvizzu.types.d.ts

--- tools/ci/run/pkg-build-ts.sh
+++ tools/ci/run/pkg-build-ts.sh
@@ -6,3 +6,11 @@ node tools/ci/type/gen-presets.cjs
 node tools/ci/type/gen-dts.cjs
 node tools/ci/type/gen-canvas-ts.cjs
 npx tsc -p src/apps/weblib/ts-api/tsconfig.json
+find "./src/apps/weblib/ts-api/" -type f -name "*.d.ts" -exec bash -c '
+    for file do
+        relative_path=$(realpath --relative-to="./src/apps/weblib/ts-api/" "$file")
+        destination_path="./dist/$relative_path"
+        mkdir -p "$(dirname "$destination_path")"
+        cp "$file" "$destination_path"
+    done
+' bash {} +
