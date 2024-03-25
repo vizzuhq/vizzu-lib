3a3f0059163a9e4ae2a08f7436b21fd9087b2f43
Bela Schaum <schaumb@gmail.com>
2024-03-25 10:56:01 +0100

    fix new test format

--- test/e2e/tests/fixes/55278793.mjs
+++ test/e2e/tests/fixes/55278793.mjs
@@ -1,22 +1,22 @@
 const testSteps = [
-  (chart) =>
-    chart.animate({
-      data: {
-        "series": [
-          {
-            "name": "D1",
-            "values": ["v1", "v2"]
-          },
-          {
-            "name": "M1",
-            "values": [-1, -2]
-          }
-        ]
-      },
-      config: {
-        "size": ["D1", "M1"]
-      }
-    })
+	(chart) =>
+		chart.animate({
+			data: {
+				series: [
+					{
+						name: 'D1',
+						values: ['v1', 'v2']
+					},
+					{
+						name: 'M1',
+						values: [-1, -2]
+					}
+				]
+			},
+			config: {
+				size: ['D1', 'M1']
+			}
+		})
 ]
 
 export default testSteps
