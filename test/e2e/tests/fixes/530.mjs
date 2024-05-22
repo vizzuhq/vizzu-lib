cead275f1a80eb0006a3b63b76dea08e50130b12
Bela Schaum <schaumb@gmail.com>
2024-05-22 22:09:31 +0200

    reformat

--- test/e2e/tests/fixes/530.mjs
+++ test/e2e/tests/fixes/530.mjs
@@ -1,45 +1,44 @@
 const testSteps = [
-  (chart) => chart.animate({
-    data: {
-      series: [
-        {
-          name: 'Year',
-          type: 'dimension',
-          values: ['2005', 'Total']
-        },
-        {
-          name: 'Result',
-          type: 'dimension',
-          values: ['Won', 'Lost']
-        }
-      ]
-    }
-  }),
-  (chart) => chart.animate({
-    data: {
-      filter: (record) => {
-        return record.Year !== 'Total'
-      }
-    },
-    config: {
-      x: 'Year'
-    }
-  }),
+	(chart) =>
+		chart.animate({
+			data: {
+				series: [
+					{
+						name: 'Year',
+						type: 'dimension',
+						values: ['2005', 'Total']
+					},
+					{
+						name: 'Result',
+						type: 'dimension',
+						values: ['Won', 'Lost']
+					}
+				]
+			}
+		}),
+	(chart) =>
+		chart.animate({
+			data: {
+				filter: (record) => {
+					return record.Year !== 'Total'
+				}
+			},
+			config: {
+				x: 'Year'
+			}
+		}),
 
-  (chart) => chart.animate(
-    {
-      data: {
-        filter: (record) => {
-          return (
-            record.Year === 'Total'
-          )
-        }
-      },
-      config: {
-        y: 'Result'
-      }
-    }
-  )
+	(chart) =>
+		chart.animate({
+			data: {
+				filter: (record) => {
+					return record.Year === 'Total'
+				}
+			},
+			config: {
+				y: 'Result'
+			}
+		})
 ]
 
 export default testSteps
