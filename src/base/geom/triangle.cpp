d8d2ce87f0ff21df97344076b5c5a915328ab59f
Bela Schaum <schaumb@gmail.com>
2024-07-10 14:50:38 +0200

    Fix ConvexQuad::Isosceles on same point + some refactor

--- src/base/geom/triangle.cpp
+++ src/base/geom/triangle.cpp
@@ -3,13 +3,6 @@
 namespace Geom
 {
 
-Triangle::Triangle(const Point &p0, const Point &p1, const Point &p2)
-{
-	points[0] = p0;
-	points[1] = p1;
-	points[2] = p2;
-}
-
 double Triangle::area() const
 {
 	auto A = points[2] - points[1];
