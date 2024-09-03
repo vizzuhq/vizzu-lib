d460de3c0469bcb16d4ed69383e094b850607b0a
Bela Schaum <schaumb@users.noreply.github.com>
2024-09-03 20:10:12 +0200

    Move to clang

--- tools/ci/run/pkg-build-desktop-clangformat.sh
+++ tools/ci/run/pkg-build-desktop-clangformat.sh
@@ -9,16 +9,16 @@ JOBS="$1"
 if [ -z "$1" ]; then
     JOBS=$DEFAULT_JOBS
 fi
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
-mkdir -p build/cmake-desktop-gcc
-pushd build/cmake-desktop-gcc
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+mkdir -p build/cmake-desktop-clang
+pushd build/cmake-desktop-clang
 cmake -Dclangformat:BOOL="ON" ../../project/cmake/
 cmake --build . -- --jobs=$JOBS
 popd
 
 # test desktop
 
-pushd build/cmake-desktop-gcc/test
+pushd build/cmake-desktop-clang/test
 ./vizzutest
 popd
