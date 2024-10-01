f2a862e7b308e4942beb12c1272afbbe6c31517d
Bela Schaum <schaumb@gmail.com>
2024-10-01 19:43:44 +0200

    Remove align max

--- src/chart/options/align.h
+++ src/chart/options/align.h
@@ -10,30 +10,17 @@ namespace Vizzu::Base
 
 struct Align : private Math::Range<double>
 {
-public:
-	enum class Type : std::uint8_t {
-		none,
-		min,
-		center,
-		max,
-		stretch
-	};
+	enum class Type : std::uint8_t { none, min, center, stretch };
 
-	Align(Type type, const Range<double> &range) :
-	    Range<double>(range),
-	    type(type)
-	{}
+	Align(Type type, const Range &range) : Range(range), type(type) {}
 
-	[[nodiscard]] Range<double> getAligned(
-	    const Range<double> &range) const
+	[[nodiscard]] Range getAligned(const Range &range) const
 	{
 		switch (type) {
 		case Type::none: return range;
 		case Type::min: return range + min - range.getMin();
 		case Type::center: return range - range.middle() + middle();
-		case Type::max: return range + max - range.getMax();
-		case Type::stretch:
-			return static_cast<const Range<double> &>(*this);
+		case Type::stretch: return static_cast<const Range &>(*this);
 		}
 		throw std::logic_error("invalid align type");
 	}
