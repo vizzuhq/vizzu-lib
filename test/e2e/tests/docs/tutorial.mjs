fe3a18353586106c18768536dd492fe780b46041
David Vegh <veghdev@gmail.com>
2024-11-04 11:41:46 +0100

    refactor tutorial test

---
+++ test/e2e/tests/docs/tutorial.mjs
@@ -0,0 +1,18 @@
+import MdChart from '../../../../docs/assets/javascripts/mdchart.js'
+
+async function getTestSteps(dataFile, configName) {
+	const dataLoaded = await import(dataFile)
+	const configLoaded = await import(`../../../../docs/tutorial/${configName}/config.js`)
+	const [data, config] = await Promise.all([dataLoaded, configLoaded])
+
+	const animations = await MdChart.loadAnimations(
+		config.default,
+		`./docs/tutorial/${configName}`,
+		`../../../../../../docs/tutorial/${configName}`
+	)
+	animations.unshift((chart) => chart.animate({ data: data.default }))
+
+	return animations.flat(Infinity)
+}
+
+export default getTestSteps
--- test/e2e/tests/docs/tutorial/axes_title_tooltip.mjs
+++ test/e2e/tests/docs/tutorial/axes_title_tooltip.mjs
@@ -1,16 +1,8 @@
-import MdChart from '../../../../../docs/assets/javascripts/mdchart.js'
+import getTestSteps from '../tutorial.mjs'
 
-const dataLoaded = await import('../../../../../docs/assets/data/music_data.js')
-const configLoaded = await import('../../../../../docs/tutorial/axes_title_tooltip/config.js')
-const [data, config] = await Promise.all([dataLoaded, configLoaded])
-
-const animations = await MdChart.loadAnimations(
-	config.default,
-	'./docs/tutorial/axes_title_tooltip',
-	'../../../../../../docs/tutorial/axes_title_tooltip'
+const testSteps = await getTestSteps(
+	'../../../../docs/assets/data/music_data.js',
+	'axes_title_tooltip'
 )
-animations.unshift((chart) => chart.animate({ data: data.default }))
-
-const testSteps = animations.flat(Infinity)
 
 export default testSteps
