bf6005b13aedfac6f0f87cd271bc3718b5930e5d
Bela Schaum <schaumb@gmail.com>
2024-11-08 23:21:09 +0100

    Fix clang-tidy

--- src/chart/generator/axis.cpp
+++ src/chart/generator/axis.cpp
@@ -85,13 +85,13 @@ Geom::Point Axises::origo() const
 }
 
 MeasureAxis::MeasureAxis(const Math::Range<double> &interval,
-    const std::string &series,
+    std::string series,
     const std::string_view &unit,
     const std::optional<double> &step) :
     enabled(true),
     range(interval.isReal() ? interval
                             : Math::Range<double>::Raw(0, 0)),
-    series(series),
+    series(std::move(series)),
     unit(std::string{unit}),
     step(step ? *step : Math::Renard::R5().ceil(range.size() / 5.0))
 {
