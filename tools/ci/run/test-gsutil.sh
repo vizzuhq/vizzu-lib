d4c0beebb765669939a7548989ab621f3b65224a
David Vegh <veghdev@gmail.com>
2025-02-10 17:47:22 +0100

    run tests with 4 browser

--- tools/ci/run/test-gsutil.sh
+++ tools/ci/run/test-gsutil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SHORT_SHA="$1"
-npm test -- -b 5 --vizzu /dist/vizzu.min.js --Werror sameref --hashes ALL --maxFailedImages 100 || TEST_FAILED=true
+npm test -- -b 4 --vizzu /dist/vizzu.min.js --Werror sameref --hashes ALL --maxFailedImages 100 || TEST_FAILED=true
 tar -czvf test/e2e/test_report.tgz test/e2e/test_report/ || TGZ_FAILED=true
 gsutil cp "test/e2e/test_report.tgz" "gs://vizzu-lib-main-sha/lib-$SHORT_SHA/test_report.tgz" || GSUTIL_FAILED=true
 if [ "$TEST_FAILED" == "true" ] || [ "$TGZ_FAILED" == "true" ] || [ "$GSUTIL_FAILED" == "true" ]; then
