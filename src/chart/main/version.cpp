f8d8d6943118ec12b0798b457db4bd00598c0533
David Vegh <veghdev@gmail.com>
2025-04-03 11:58:40 +0200

    Set version to 0.16.2

--- src/chart/main/version.cpp
+++ src/chart/main/version.cpp
@@ -2,6 +2,6 @@
 
 #include "base/app/version.h"
 
-const App::Version Vizzu::Main::version(0, 16, 1);
+const App::Version Vizzu::Main::version(0, 16, 2);
 
 const char *const Vizzu::Main::siteUrl = "https://vizzu.io/";
